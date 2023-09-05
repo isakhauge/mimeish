964ecb31ee5dba7cb9bd4c2eae09adf8212af499
Isak K. Hauge <isakhauge@icloud.com>
2023-09-05 13:17:53 +0200

    Fix global types

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,3 +1,8 @@
+export const lookup: {
+	mime: (path: string) => string | undefined
+	extension: (mime: string) => string | undefined
+}
+
 declare module mimeish {
 	export const lookup: {
 		mime: (path: string) => string | undefined
